205cd30b910fb8ae549562978136d9126c4522fa
pthav <pta2@rice.edu>
2024-07-27 23:38:13 -0500

    User can now change number of cells in the grid

--- code.js
+++ code.js
@@ -1,18 +1,11 @@
-// Wait for the dom to be created before creating the grid
-function color_cell(e){
-    e.target.style.backgroundColor = 'black';
-};
 
-window.addEventListener('DOMContentLoaded', function() {
-    // Query container and button then attach listeners
-    const container = document.querySelector(".container");
-    container.addEventListener("mouseover", color_cell);
-    
-    // Create the grid
-    let cells = 16;
+let container;
+let button;
+
+// Draw the grid where cells defines how many cells for each dimension
+function draw_grid(cells){
     let cell_width = 480 / cells;
     for(let i = 0; i < cells * cells; i++){
-        // Define cell
         const current_cell = document.createElement("div");
         current_cell.style.width = `${cell_width}px`;
         current_cell.style.height = `${cell_width}px`;
@@ -22,4 +15,31 @@ window.addEventListener('DOMContentLoaded', function() {
         
         container.appendChild(current_cell);
     }
+}
+
+function delete_grid(){
+    const cells = document.querySelectorAll(".container > div");
+    cells.forEach((cell) => cell.remove());
+}
+
+
+function color_cell(e){
+    e.target.style.backgroundColor = 'black';
+};
+
+function change_num_cells(e){
+    answer = parseInt(prompt("How many cells per side?"));
+    delete_grid();
+    draw_grid(answer);
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    // Query container and button then attach listeners
+    container = document.querySelector(".container");
+    button = document.querySelector("button");
+    container.addEventListener("mouseover", color_cell);
+    button.addEventListener("mousedown", change_num_cells);
+    
+    // Create the grid
+    draw_grid(16);
 });
