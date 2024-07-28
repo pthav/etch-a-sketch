c4d71099c6b77ae2aa48433dcfd462bb2b5f47ce
pthav <pta2@rice.edu>
2024-07-28 00:09:18 -0500

    Filling changed to rainbow and opacity increases by 0.1 every hover

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 
 let container;
 let button;
+const cell_dimension_limit = 100;
 
 // Draw the grid where cells defines how many cells for each dimension
 function draw_grid(cells){
@@ -11,26 +12,38 @@ function draw_grid(cells){
         current_cell.style.height = `${cell_width}px`;
         current_cell.style.padding = 0;
         current_cell.style.margin = 0;
-        current_cell.style.backgroundColor = "white";
-        
+        current_cell.style.backgroundColor = generate_color();
+        current_cell.style.opacity = 0;
         container.appendChild(current_cell);
     }
-}
+};
 
 function delete_grid(){
     const cells = document.querySelectorAll(".container > div");
     cells.forEach((cell) => cell.remove());
-}
-
+};
 
 function color_cell(e){
-    e.target.style.backgroundColor = 'black';
+    // Increase the opacity
+    e.target.style.opacity = Math.min(1,parseFloat(window.getComputedStyle(e.target).getPropertyValue("opacity")) + 0.1);
+    container.style.backgroundColor = "transparent";
+    container.style.opacity = 1;
+    console.log(e.target.style.opacity);
+};
+
+function generate_color(){
+    let r = Math.random() * 255
+    let g = Math.random() * 255
+    let b = Math.random() * 255
+    return `rgb(${r.toFixed()},${g.toFixed()},${b.toFixed()})`
 };
 
 function change_num_cells(e){
     answer = parseInt(prompt("How many cells per side?"));
-    delete_grid();
-    draw_grid(answer);
+    if(answer <= cell_dimension_limit){
+        delete_grid();
+        draw_grid(answer);
+    }
 }
 
 window.addEventListener('DOMContentLoaded', function() {
